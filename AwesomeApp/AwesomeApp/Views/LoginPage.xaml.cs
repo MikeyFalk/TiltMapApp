071084772984ba95f0bb4d05d146bcf6a0268962
Mike Falk <falk478@yahoo.com>
2021-03-02 21:58:08 -0800

    Add project files.

---
+++ AwesomeApp/AwesomeApp/Views/LoginPage.xaml.cs
@@ -0,0 +1,26 @@
+﻿using AwesomeApp.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace AwesomeApp.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class LoginPage : ContentPage
+    {
+        public LoginPage()
+        {
+            InitializeComponent();
+            this.BindingContext = new LoginViewModel();
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
